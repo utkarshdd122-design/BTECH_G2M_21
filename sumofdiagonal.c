b368a9e3a560f3135aef0cbc98ef1da747ebca93
utkarshdd122-design <utkarshdd122@gmail.com>
2026-04-03 14:39:23 +0530

    Add files via upload

---
+++ sumofdiagonal.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the size of the square matrix: ");
+    scanf("%d", &n);
+    
+    int matrix[n][n];
+    printf("Enter the elements of the matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += matrix[i][i];
+    }
+    
+    printf("Sum of diagonal elements: %d\n", sum);
+    return 0;
+}
